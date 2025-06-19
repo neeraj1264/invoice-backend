5fc7e635b2da410686c9a509374d457bc8211241
NeerajQA <neeraj1264qa@gmail.com>
2025-06-19 12:49:06 +0530

    add delivery and discount field

--- api/index.js
+++ api/index.js
@@ -27,5 +27,8 @@ app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/customerdata', customerdataRoutes);
+
+// const PORT = process.env.PORT || 5000; 
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
  
 module.exports = app;
--- api/models/order.js
+++ api/models/order.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  id: { type: String, required: true },
+  id: { type: String},
   products: [{ name: String, price: Number, quantity: Number , size: String}],
   totalAmount: { type: Number, required: true },
   name: { type: String },
   phone: { type: Number },
   address: { type: String },
   timestamp: { type: String, required: true },
+  discount:{ type: Number },
+  delivery: { type: Number }
 });
 
 const Order = mongoose.model('Order', orderSchema);
